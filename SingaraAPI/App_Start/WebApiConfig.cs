0fab2b30fa947408aac060f14b7fc73b3ee2cf92
Tilak J <tilak.j0321@outlook.com>
2023-10-03 09:46:45 +0530

    Add project files.

---
+++ SingaraAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿//using Microsoft.AspNetCore.Cors;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace SingaraAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var cors = new EnableCorsAttribute("*", "*", "*"); // Allow requests from any origin, any header, and any method (not recommended for production)
+            config.EnableCors(cors);
+            // Web API routes
+            config.Formatters.Clear(); // Remove other formatters
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SingaraAPI/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using SingaraAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace SingaraAPI.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private static List<Item> data = new List<Item>()
+        {
+          new Item() { Id = 1, Value = "item1" },
+          new Item() { Id = 2, Value = "item2" }
+        };
+
+        public IEnumerable<Item> Get() => data;
+
+        public IHttpActionResult Get(int id) => id >= 0 && id < data.Count ? Ok(data.Find(c => c.Id == id)) : (IHttpActionResult)NotFound();
+
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] Item item)
+        {
+            if (string.IsNullOrEmpty(item.Value))
+                return BadRequest("Value cannot be empty.");
+            int newId = data.Count > 0 ? data.Max(c => c.Id) : 0;
+            data.Add(new Item()
+            {
+                Id = newId + 1,
+                Value = item.Value
+            });
+            return Ok(item.Value);
+        }
+
+        public IHttpActionResult Put(int id, [FromBody] Item item)
+        {
+            if (id < 0 || id >= data.Count || string.IsNullOrEmpty(item.Value))
+                return NotFound();
+            data.Find(c => c.Id == id).Value = item.Value;
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            Item obj = data.Find(c => c.Id == id);
+            if (obj == null)
+                return NotFound();
+            data.Remove(obj);
+            return Ok();
+        }
+    }
+}
